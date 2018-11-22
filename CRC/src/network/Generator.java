08b44aa6d693add05ab424077025716b8940eb4d
DavidIbrahim <davidibrahim8888@gmail.com>
2018-11-22 19:26:28 +0200

    refactoring

--- CRC/src/network/Generator.java
+++ CRC/src/network/Generator.java
@@ -13,54 +13,70 @@ public class Generator {
 
 
 
-    private String calculateRemainder(String x){
-        BitSet Message =new BitSet();
+    private String calculateRemainder(String generatorString,String messageString){
+        BitSet messageBitSet =new BitSet();
         BitSet generatorBitset =new BitSet();
-        int G_length=generatorFn.length();
-        int M_length=x.length();
-        String Rem="";
+        int generatorLength=generatorString.length();
+        int messageLength=messageString.length();
+        String remainder="";
+
+
+
         // convert msg string 2 bitset
-        for(int i=0;i<M_length;i++)
-            if(x.charAt(i)=='1')
-                Message.set(M_length-i-1);
+        for(int i=0;i<messageLength;i++)
+            if(messageString.charAt(i)=='1')
+                messageBitSet.set(messageLength-i-1);
 
         // convert generator string 2 bitset
-        for(int i=0;i<G_length;i++)
-            if(generatorFn.charAt(i)=='1')
-                generatorBitset.set(M_length-i-1);
+        for(int i=0;i<generatorLength;i++)
+            if(generatorString.charAt(i)=='1')
+                generatorBitset.set(messageLength-i-1);
 
         // xor operation
 
-        while(Message.length()>=G_length)
+        while(messageBitSet.length()>=generatorLength)
         {
-            if(Message.length()==generatorBitset.length())
-                Message.xor(generatorBitset);
+            if(messageBitSet.length()==generatorBitset.length())
+                messageBitSet.xor(generatorBitset);
 
             for(int i=0;i<generatorBitset.length();i++)
                 generatorBitset.set(i,generatorBitset.get(i+1));
 
         }
         // convert reminder bit set  2 string
-         for(int i=1;i<G_length;i++)
-            if(Message.get(G_length-i-1))
-                Rem=Rem+"1";
+         for(int i=1;i<generatorLength;i++)
+            if(messageBitSet.get(generatorLength-i-1))
+                remainder=remainder+"1";
             else
-                Rem=Rem+"0";
+                remainder=remainder+"0";
 
 
 
-        return Rem;
+        return remainder;
     }
 
+
+    /**
+     *  used by the receiver of the message to verify it's correct or no
+     * @param messageString
+     * @return true if there's no error in message
+     */
+
     public boolean verify(String messageString) {
-        String remainder = calculateRemainder(messageString);
+        String remainder = calculateRemainder(generatorFn,messageString);
 
         return Integer.valueOf(remainder)==0;
     }
 
+    /**
+     * used by sender to produce the message to be sent after adding the remainder of the generator
+     *
+     */
+
     public String generateMessage(String message) {
         String zero = "0";
-        String remainder = calculateRemainder(message+new String(new char[generatorFn.length()-1]).replace("\0", zero));
+        String messageWithZerosConcatenated = message+new String(new char[generatorFn.length()-1]).replace("\0", zero);
+        String remainder = calculateRemainder(generatorFn,messageWithZerosConcatenated);
         return message+remainder;
     }
 }
